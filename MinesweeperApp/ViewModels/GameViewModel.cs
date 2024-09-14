8416e822970014dbd37661ff693aee42a99ede43
Platypus112 <idancar7@gmail.com>
2024-09-14 18:35:29 +0300

    asfikojughh

--- MinesweeperApp/ViewModels/GameViewModel.cs
+++ MinesweeperApp/ViewModels/GameViewModel.cs
@@ -2,6 +2,7 @@
 using MinesweeperApp.Services;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,16 +11,43 @@ namespace MinesweeperApp.ViewModels
 {
     public class GameViewModel:ViewModel
     {
-        public Tile[,] Board { get { return board; } set {board=value; OnPropertyChanged();} }
-        private Tile[,] board;
-        public int Height { get { return Height; } set { Height = value; OnPropertyChanged(); } }
-        public int Width { get { return Width; } set { Width = value; OnPropertyChanged(); } }
+        public DataTable Board { get { return board; } set {board=value; OnPropertyChanged();} }
+        private DataTable board;
+        public int Height { get { return height; } set { height = value; OnPropertyChanged(); } }
+        private int height;
+        public int Width { get { return width; } set { width = value; OnPropertyChanged(); } }
+        private int width;
         private readonly Service service;
         private readonly Game game;
         public GameViewModel()
         {
             game = new Game(5, 5, 4);
+            Width = 5;
+            Height = 5;
+            board = ConvertBoardToDatatable();
         }
-
+        private DataTable ConvertBoardToDatatable()
+        {
+            DataTable result = new DataTable();
+            for(int i = 0; i < Width; i++)
+            {
+                DataColumn column = new DataColumn();
+                column.DataType=typeof(Tile);
+                column.Unique = false;
+                column.ColumnName = i.ToString();
+                result.Columns.Add(column);
+            }
+            for (int i = 0; i < Height; i++)
+            {
+                DataRow row = result.NewRow();
+                for (int j = 0; j < Width; j++)
+                {
+                    row[j.ToString()] = game.GetBoardState()[i,j];
+                }
+                result.Rows.Add(row);
+            }
+            return result;
+        }
+    
     }
 }

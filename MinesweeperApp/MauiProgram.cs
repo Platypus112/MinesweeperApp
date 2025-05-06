61b53a6fc3242edd1112d33f01e8f5394a1bf11a
Platypus112 <idancar7@gmail.com>
2025-05-06 17:03:46 +0300

    sdk....

--- MinesweeperApp/MauiProgram.cs
+++ MinesweeperApp/MauiProgram.cs
@@ -27,24 +27,24 @@ namespace MinesweeperApp
             #endregion
 
             #region add ViewModels
-            builder.Services.AddTransient<GameViewModel>();
-            builder.Services.AddTransient<HomeViewModel>();
+            builder.Services.AddTransient<AdminViewModel>();
             builder.Services.AddTransient<AppShellViewModel>();
-            builder.Services.AddTransient<LoginViewModel>();
-            builder.Services.AddTransient<RegisterViewModel>();
-            builder.Services.AddTransient<StartGameViewModel>();
-            builder.Services.AddTransient<ProfileViewModel>();
-            builder.Services.AddTransient<LeaderboardViewModel>();
-            builder.Services.AddTransient<GameReportsViewModel>();
-            builder.Services.AddTransient<FriendLeaderboardViewModel>();
-            builder.Services.AddTransient<TimelyLeaderboardViewModel>();
-            builder.Services.AddTransient<SocialPageViewModel>();
-            builder.Services.AddTransient<FriendRequestsViewModel>();
             builder.Services.AddTransient<BlockedListViewModel>();
             builder.Services.AddTransient<EditProfileViewModel>();
-            builder.Services.AddTransient<AdminViewModel>();
-            builder.Services.AddTransient<UserReportsViewModel>();
+            builder.Services.AddTransient<FriendLeaderboardViewModel>();
+            builder.Services.AddTransient<FriendRequestsViewModel>();
+            builder.Services.AddTransient<GameReportsViewModel>();
+            builder.Services.AddTransient<GameViewModel>();
+            builder.Services.AddTransient<HomeViewModel>();
+            builder.Services.AddTransient<LeaderboardViewModel>();
+            builder.Services.AddTransient<LoginViewModel>();
             builder.Services.AddTransient<LogoutViewModel>();
+            builder.Services.AddTransient<ProfileViewModel>();
+            builder.Services.AddTransient<RegisterViewModel>();
+            builder.Services.AddTransient<StartGameViewModel>();
+            builder.Services.AddTransient<SocialPageViewModel>();
+            builder.Services.AddTransient<TimelyLeaderboardViewModel>();
+            builder.Services.AddTransient<UserReportsViewModel>();
             #endregion
 
             #region add Service
@@ -52,24 +52,24 @@ namespace MinesweeperApp
             #endregion
 
             #region add Views
-            builder.Services.AddTransient<LoginView>();
-            builder.Services.AddTransient<GameView>();
-            builder.Services.AddTransient<HomeView>();
+            builder.Services.AddTransient<AdminView>();
             builder.Services.AddTransient<AppShell>();
-            builder.Services.AddTransient<RegisterView>();
-            builder.Services.AddTransient<StartGameView>();
-            builder.Services.AddTransient<ProfileView>();
-            builder.Services.AddTransient<LeaderboardView>();
-            builder.Services.AddTransient<GameReportsView>();
-            builder.Services.AddTransient<FriendLeaderboardView>();
-            builder.Services.AddTransient<TimelyLeaderboardView>();
-            builder.Services.AddTransient<SocialPageView>();
-            builder.Services.AddTransient<FriendRequestsView>();
             builder.Services.AddTransient<BlockedListView>();
             builder.Services.AddTransient<EditProfileView>();
-            builder.Services.AddTransient<AdminView>();
-            builder.Services.AddTransient<UserReportsView>();
+            builder.Services.AddTransient<FriendLeaderboardView>();
+            builder.Services.AddTransient<FriendRequestsView>();
+            builder.Services.AddTransient<GameReportsView>();
+            builder.Services.AddTransient<GameView>();
+            builder.Services.AddTransient<HomeView>();
+            builder.Services.AddTransient<LeaderboardView>();
+            builder.Services.AddTransient<LoginView>();
             builder.Services.AddTransient<LogoutView>();
+            builder.Services.AddTransient<ProfileView>();
+            builder.Services.AddTransient<RegisterView>();
+            builder.Services.AddTransient<StartGameView>();
+            builder.Services.AddTransient<SocialPageView>();
+            builder.Services.AddTransient<TimelyLeaderboardView>();
+            builder.Services.AddTransient<UserReportsView>();
             #endregion
 
 #if DEBUG
